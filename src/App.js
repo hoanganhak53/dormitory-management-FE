6a5667b21e615302e1883a74a8ecdf5463e51a93
Hoàng Anh <82218114+hoanganhak53@users.noreply.github.com>
2023-06-26 10:11:04 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDFVNWoFElQIwnpQf5NRgBpQmh_mR7Q27w',
-    authDomain: 'prj3-371008.firebaseapp.com',
-    projectId: 'prj3-371008',
-    storageBucket: 'prj3-371008.appspot.com',
-    messagingSenderId: '205258461086',
-    appId: '1:205258461086:web:993e463c1cc422c6c3908f'
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID'
 };
 
 const app = initializeApp(firebaseConfig);
